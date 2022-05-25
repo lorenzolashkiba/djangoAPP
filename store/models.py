cef1d695bf5242be70157dac28ab40584981ce0d
lorenzolashkiba <lorenzoboiko@gmail.com>
2022-05-25 10:43:17 +0200

    first commit

---
+++ store/models.py
@@ -0,0 +1,66 @@
+from bisect import bisect_right
+from typing import Collection
+from django.db import models
+
+# Create your models here.
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+class Cart(models.Model):
+    id = models.BigAutoField(auto_created=True,primary_key=True,serialize=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    feature_product = models.ForeignKey('Product',on_delete=models.SET_NULL,null=True,related_name='+')
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    slug = models.SlugField()
+    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateField(auto_now=True)
+    collection = models.ForeignKey(Collection,on_delete=models.CASCADE)
+    promotion = models.ManyToManyField(Promotion)
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE,'Bronze'),
+        (MEMBERSHIP_SILVER,'Silver'),
+        (MEMBERSHIP_GOLD,'Gold')
+    ]
+    
+    given_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.IntegerField()
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1,choices=MEMBERSHIP_CHOICES,default=MEMBERSHIP_BRONZE)
+
+    class Meta:
+        db_table = 'store_customers'
+
+class Order(models.Model):    
+    PAYMENT_PENDING = 'P'
+    PAYMENT_COMPLETE = 'C'
+    PAYMENT_FAILED = 'F'
+    PAYMENT_STATUS = [
+        (PAYMENT_PENDING,"Pending"),
+        (PAYMENT_COMPLETE,"Complete"),
+        (PAYMENT_FAILED,"Failed")
+    ]
+    placed_at = models.DateTimeField(auto_now_add= True)
+    payment_status = models.CharField(choices = PAYMENT_STATUS,max_length=1,default=PAYMENT_PENDING)
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer,on_delete=models.CASCADE,primary_key=True)
+    zip = models.IntegerField()
+
